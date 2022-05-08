ebafa6975198a678638bb77531aeccc88288aa21
w1zzyl <w1zzyl@protonmail.com>
2022-05-09 01:16:48 +0530

    sm8150-common: Fix errors
    Signed-off-by: w1zzyl <w1zzyl@protonmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -447,10 +447,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
